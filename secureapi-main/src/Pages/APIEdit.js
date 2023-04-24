40fd916a28dc9db369e919f24f09205540207fee
Sunnyshineshow <112595512+SIIT-Sunnyshineshow@users.noreply.github.com>
2023-04-25 01:47:01 +0700

    Jaja's debug 4

--- secureapi-main/src/Pages/APIEdit.js
+++ secureapi-main/src/Pages/APIEdit.js
@@ -1,33 +1,29 @@
 import { useState } from "react";
 
-
-
-
 function APIEdit() {
-    const [linkUrl, setLinkUrl] = useState('');
+  const [linkUrl, setLinkUrl] = useState("");
 
-<div>
-    <h1 style={{ textAlign: "right", marginRight: "10px" }}>
-        Posting API
-    </h1>
-    <h3 style={{textAlign: "left", marginLeft: "20px"}}>
-        API Gateway <br/>
-    </h3>
+  return (
     <div>
-    <input
+      <h1 style={{ textAlign: "right", marginRight: "10px" }}>Posting API</h1>
+      <h3 style={{ textAlign: "left", marginLeft: "20px" }}>
+        API Gateway <br />
+      </h3>
+      <div>
+        <input
           type="text"
           value={linkUrl}
           onChange={(event) => setLinkUrl(event.target.value)}
         />
 
-        {linkUrl.startsWith('http') && (
+        {linkUrl.startsWith("http") && (
           <a href={linkUrl} target="_blank" rel="noopener noreferrer">
             Click here to view link
           </a>
         )}
+      </div>
     </div>
-
-</div>
+  );
 }
 
 export default APIEdit;
